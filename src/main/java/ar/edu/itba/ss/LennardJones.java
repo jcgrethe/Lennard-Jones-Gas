0942443d7b3379b3348bc5d60657842513f09e0f
juan <jgrethe@itba.edu.ar>
2019-04-25 19:07:28 -0300

    project set

---
+++ src/main/java/ar/edu/itba/ss/LennardJones.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.ss;
+
+/**
+ * Hello world!
+ *
+ */
+public class LennardJones
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
