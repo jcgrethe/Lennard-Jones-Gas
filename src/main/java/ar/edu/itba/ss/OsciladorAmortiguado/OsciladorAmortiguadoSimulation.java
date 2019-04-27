a6bb5985e4bbb74e850beba160c911d0626a7de5
Martin Grabina <mgrabina@itba.edu.ar>
2019-04-27 13:56:53 -0300

    Oscillation simulation structure

--- src/main/java/ar/edu/itba/ss/OsciladorAmortiguado/OsciladorAmortiguadoSimulation.java
+++ src/main/java/ar/edu/itba/ss/OsciladorAmortiguado/OsciladorAmortiguadoSimulation.java
@@ -1,10 +1,12 @@
 package ar.edu.itba.ss.OsciladorAmortiguado;
 
-import ar.edu.itba.ss.Algorithms.Algorithm;
-import ar.edu.itba.ss.Algorithms.Beeman;
-import ar.edu.itba.ss.Algorithms.GearPredictor;
-import ar.edu.itba.ss.Algorithms.Verlet;
+import ar.edu.itba.ss.Algorithms.*;
+import ar.edu.itba.ss.io.Output;
 import ar.edu.itba.ss.models.Particle;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Hello world!
@@ -12,24 +14,71 @@ import ar.edu.itba.ss.models.Particle;
  */
 public class OsciladorAmortiguadoSimulation {
 
-    private Integer A = 1;       // TODO: Put real value
+    private static Integer A = 1;       // TODO: Put real value
 
     // Default Paramenters
-    private Double M = 70.0;     // In Kilogrames
-    private Integer K = 10000;   // In N/m
-    private Double y = 100.0;    // In kg/s
-    private Double Tf = 5.0;     // In s
+    private static Double M = 70.0;     // In Kilogrames
+    private static Integer K = 10000;   // In N/m
+    private static Double y = 100.0;    // In kg/s
+    private static Double Tf = 5.0;     // In s
 
     // Initial Conditions
-    private Double initialX = 1.0;          // In m
-    private Double initialV = -A*y/(2*M);   // In m/s
+    private static Double initialX = 1.0;          // In m
+    private static Double initialV = -A*y/(2*M);   // In m/s
+
+    private static Double endTime = 10.0;          // In s
 
     public static void main( String[] args )
     {
-        Particle particle = new Particle(0,0,0,0,0,0);
-        Algorithm verletAlgorithm = new Verlet();
-        Algorithm BeemanAlgorithm = new Beeman();
-        Algorithm GearPredictorAlgorithm = new GearPredictor();
-        System.out.println( "Hello World!" );
+        Double[] dts = {0.0001, 0.001, 0.01, 0.1};
+        List<Double> diferentials = Arrays.asList(dts);
+        diferentials.sort(Comparator.comparingDouble(Double::doubleValue));
+//        diferentials.sort(Comparator.comparingDouble(Double::doubleValue).reversed());
+
+        double[] beenmanError = new double[diferentials.size()];
+        double[] gearPredictorError = new double[diferentials.size()];
+        double[] verletError = new double[diferentials.size()];
+        double[][] analitycPositions = null;
+        double[][] beenmanPositions = null;
+        double[][] gearPredictorPositions = null;
+        double[][] verletPositions = null;
+
+        for (double diferential_t : diferentials){
+            int index = diferentials.indexOf(diferential_t);
+
+            analitycPositions[index] = oscillation(new Analityc(), diferential_t, endTime);
+            beenmanPositions[index] = oscillation(new Beeman(), diferential_t, endTime);
+            gearPredictorPositions[index] = oscillation(new GearPredictor(), diferential_t, endTime);
+            verletPositions[index] = oscillation(new Verlet(), diferential_t, endTime);
+
+            beenmanError[index] = meanSquaredError(analitycPositions[index], beenmanPositions[index]);
+            gearPredictorError[index] = meanSquaredError(analitycPositions[index], gearPredictorPositions[index]);
+            verletError[index] = meanSquaredError(analitycPositions[index], verletPositions[index]);
+        }
+        Output.printOscillationsResults(analitycPositions,beenmanPositions,gearPredictorPositions,verletPositions,beenmanError,gearPredictorError,verletError,diferentials);
+    }
+
+    public static double meanSquaredError(double[] one, double[] another) {
+        double sum = 0;
+        for(int i = 0; i < one.length; i++) {
+            sum += Math.pow(one[i] - another[i], 2);
+        }
+        return sum/one.length;
+    }
+
+    private static double[] oscillation(Algorithm algorithm, double dt, double endtime){
+        double[] positions = new double[(int) (endtime/dt)];
+
+        if (algorithm instanceof Analityc){
+
+        }else if (algorithm instanceof Beeman){
+
+        }else if (algorithm instanceof GearPredictor){
+
+        }else if (algorithm instanceof Verlet){
+
+        }
+
+        return positions;
     }
 }
